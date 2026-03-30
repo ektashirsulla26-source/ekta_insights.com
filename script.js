77677ced185fe2254e3a3152a2d790f9fbea02a9
Ekta Shirsulla <ektashirsulla26@gmail.com>
2026-03-30 13:49:22 +0530

    Add typing effect for text display
    Implement typing effect for text animation.

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const text = ["Data Analyst", "Machine Learning Enthusiast", "Data Scientist"];
+let i = 0;
+let j = 0;
+let currentText = "";
+let isDeleting = false;
 
+function type() {
+    currentText = text[i];
+    
+    if (isDeleting) {
+        document.querySelector(".typing").textContent = currentText.substring(0, j--);
+    } else {
+        document.querySelector(".typing").textContent = currentText.substring(0, j++);
+    }
+
+    if (!isDeleting && j === currentText.length) {
+        isDeleting = true;
+        setTimeout(type, 1000);
+        return;
+    }
+
+    if (isDeleting && j === 0) {
+        isDeleting = false;
+        i = (i + 1) % text.length;
+    }
+
+    setTimeout(type, isDeleting ? 50 : 100);
+}
+
+type();
